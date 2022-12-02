1c92ff87367ef99565e1987385caeed4c251feec
VaiMas <vaidutee@gmail.com>
2022-12-02 11:57:48 +0200

    feat(parse): parse of json and saves data to csv file

--- car_scraper/spiders/car_pp.py
+++ car_scraper/spiders/car_pp.py
@@ -1,7 +1,8 @@
 import scrapy
 from scrapy.http.request import Request
 import time
-
+import json
+import csv
 
 
 class CarPpSpider(scrapy.Spider):
@@ -19,18 +20,63 @@ class CarPpSpider(scrapy.Spider):
         "Accept-Language": "en-US,en;q=0.5",
         "Accept": "application/json, text/plain, */*",
     }
+
     def start_requests(self):
         """Sends request to url with headers and get json file for parsing, for 
         now it is 50 urls with 20 entries each with 2s delay.
         """
         webs = []
-        for page in range (50):
+        for page in range(3):
             page += 1
-            webs.append(f'https://apipub.pp.lv/lv/api_user/v1/categories/2/lots?fV%5B22%5D%5Btype%5D=2363&orderColumn=publishDate&orderDirection=DESC&currentPage={page}&itemsPerPage=20')
-            
+            webs.append(
+                f'https://apipub.pp.lv/lv/api_user/v1/categories/2/lots?fV%5B22%5D%5Btype%5D=2363&orderColumn=publishDate&orderDirection=DESC&currentPage={page}&itemsPerPage=20')
+
         for url in webs:
             time.sleep(2)
             yield Request(url=url, headers=self.headers, callback=self.parse)
 
     def parse(self, response):
-        pass
+        """Parses json file and writes to csv with information of manufacturer,
+        model, production year, milage, VIN code and license plate.
+        """
+        data = json.loads(response.body)
+        car = []
+        try:
+            for name in data['content']['data']:
+                data_for_dictionary = {
+                    'Manufacturer': name['category']['parent']['name'],
+                    'Model': name['category']['name'],
+                    'Year': None,
+                    'Milage': None,
+                    'VIN': None,
+                    'LP': None
+                }
+
+                for value in name['adFilterValues']:
+                    if value['filter']['name'] == 'Izlaiduma gads':
+                        year = value["value"]["displayValue"]
+                        data_for_dictionary['Year'] = year
+                    if value['filter']['name'] == 'Nobraukums, km':
+                        milage = value["textValue"]
+                        data_for_dictionary['Milage'] = milage
+                    if value['filter']['name'] == 'VIN kods':
+                        vin = value["textValue"]
+                        data_for_dictionary['VIN'] = vin
+                    if value['filter']['name'] == 'Auto numurs':
+                        lp = value["textValue"]
+                        data_for_dictionary['LP'] = lp
+
+                car.append(data_for_dictionary)
+
+            with open('file.csv', 'a', encoding='utf-8', newline='') as csvfile:
+                headers = ['Manufacturer', 'Model',
+                           'Year', 'Milage', 'VIN', 'LP']
+                writer = csv.DictWriter(csvfile, fieldnames=headers)
+                csvfile.seek(0, 2)
+
+                if csvfile.tell() == 0:
+                    writer.writeheader()
+
+                writer.writerows(car)
+        except:
+            print('Something went wrong please try it again or contact developer')
